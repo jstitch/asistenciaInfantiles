53a0117c793e4e75c7e8990daac750dab51b02bf
Javier Novoa <jstitch@gmail.com>
2015-12-27 19:49:07 -0600

    Modifico templates para mostrar nombres de participantes

--- asistencia/views.py
+++ asistencia/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Participante
 
 def participantes_list(request):
-    return render(request, 'asistencia/participantes_list.html', {})
+    participantes = Participante.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'asistencia/participantes_list.html', {'participantes': participantes})
